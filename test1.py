a63c518b283f904c51ea2079eae1bc5e9f54667b
1254yuusei <1254yuusei@gmail.com>
2022-12-16 20:19:57 +0900

    初号機

--- test1.py
+++ test1.py
@@ -7,8 +7,11 @@ import plotly.graph_objects as go
 st.title('概日リズム可視化アプリ')
 
 # 時間取得
-dt_now = datetime.datetime.now()
-dt_yd = dt_now - datetime.timedelta(days=1)
+import datetime
+now = datetime.datetime.now()
+dt_now = now.strftime('%Y-%m-%d')
+yd = now - datetime.timedelta(days=35)
+dt_yd = yd.strftime('%Y-%m-%d')
 
 
 #ouraアクセストークン
@@ -17,8 +20,8 @@ access_token="QVRKWLEC4P6F7OICXAPUDX5WIVRHANEV"
 client = oura.client_pandas.OuraClientDataFrame(personal_access_token=access_token)
 
 #期間を指定（全期間が欲しい場合はこのままでOKです。）
-start_text="2022-11-16"
-end_text="2022-11-17"
+start_text = dt_yd
+end_text = dt_now
 
 #上からそれぞれ、Sleepデータ、Activityデータ、Readinessデータをdataframeに格納しています。
 data_oura=client.sleep_df(start=start_text, end=end_text)
@@ -28,11 +31,19 @@ date_start =pd.to_datetime(data_oura['bedtime_start_dt_adjusted'], format='%Y-%m
 date_end =pd.to_datetime(data_oura['bedtime_end_dt_adjusted'], format='%Y-%m-%d %H:%M:%S')
 
 #COREのデータ取得
-df = pd.read_csv('data/CORE_TEMP_1117.csv', sep = ';', header = 1,)
+df = pd.read_csv('data/CORE_data.csv', sep = ';', header = 1,)
 data = pd.to_datetime(df.iloc[:,0])
 y = df.iloc[:,1]
 plot_data = pd.DataFrame(data)
 plot_data['Temp'] = y
+
+# CORE温度差判定
+
+
+
+
+
+
 
 
 # CORE,Ouraプロット
@@ -44,13 +55,13 @@ fig.add_trace(go.Scatter(x=plot_data['DateTime'],
                         ),
                 )
 fig.add_shape(type="line",
-                        x0=data_oura['bedtime_start_dt_adjusted'][0], y0=35,
-                        x1=data_oura['bedtime_start_dt_adjusted'][0], y1=40,
+                        x0=data_oura['bedtime_start_dt_adjusted'][0], y0=36,
+                        x1=data_oura['bedtime_start_dt_adjusted'][0], y1=38,
                         line=dict(color="Red",width=3)
                 )
 fig.add_shape(type="line",
-                        x0=data_oura['bedtime_end_dt_adjusted'][0], y0=35,
-                        x1=data_oura['bedtime_end_dt_adjusted'][0], y1=40,
+                        x0=data_oura['bedtime_end_dt_adjusted'][0], y0=36,
+                        x1=data_oura['bedtime_end_dt_adjusted'][0], y1=38,
                         line=dict(color="Red",width=3)
                 )
 
